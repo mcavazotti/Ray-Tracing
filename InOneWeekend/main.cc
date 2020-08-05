0fd392184e349430a53b6a2b7177ef65e5d9c923
Matheus Cavazotti <mcavazotti@gmail.com>
2020-08-05 16:51:16 -0300

    paralellization!

--- InOneWeekend/main.cc
+++ InOneWeekend/main.cc
@@ -1,3 +1,5 @@
+#include <omp.h>
+
 #include <iostream>
 
 #include "camera.h"
@@ -10,26 +12,26 @@
 hittable_list random_scene() {
   hittable_list world;
 
-  auto ground_material = make_shared<lambertian>(color(0.5,0.5,0.5));
-  world.add(make_shared<sphere>(point3(0,-1000,0),1000, ground_material));
+  auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+  world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
 
-  for(int a = -11; a < 11; a++){
-    for(int b = -11; b < 11 ; b++) {
+  for (int a = -11; a < 11; a++) {
+    for (int b = -11; b < 11; b++) {
       auto choose_mat = random_double();
-      point3 center(a + 0.9*random_double(), 0.2, b + 0.9+random_double());
+      point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 + random_double());
 
-      if ((center - point3(4,0.2,0)).length() > 0.9) {
+      if ((center - point3(4, 0.2, 0)).length() > 0.9) {
         shared_ptr<material> sphere_material;
 
-        if(choose_mat < 0.8) {
+        if (choose_mat < 0.8) {
           // diffuse
           auto albedo = color::random() * color::random();
           sphere_material = make_shared<lambertian>(albedo);
-          world.add(make_shared<sphere>(center, 0.2,sphere_material));
+          world.add(make_shared<sphere>(center, 0.2, sphere_material));
         } else if (choose_mat < 0.95) {
           // metal
-          auto albedo = color::random(0.5,1);
-          auto fuzz = random_double(0,0.5);
+          auto albedo = color::random(0.5, 1);
+          auto fuzz = random_double(0, 0.5);
           sphere_material = make_shared<metal>(albedo, fuzz);
           world.add(make_shared<sphere>(center, 0.2, sphere_material));
         } else {
@@ -37,18 +39,18 @@ hittable_list random_scene() {
           sphere_material = make_shared<dielectric>(1.5);
           world.add(make_shared<sphere>(center, 0.2, sphere_material));
         }
-      } 
+      }
     }
   }
 
   auto material1 = make_shared<dielectric>(1.5);
-  world.add(make_shared<sphere>(point3(0,1,0),1,material1));
-  
-  auto material2 = make_shared<lambertian>(color(0.4,0.2,0.1));
-  world.add(make_shared<sphere>(point3(-4,1,0),1,material2));
-  
-  auto material3 = make_shared<metal>(color(0.7,0.6,0.5), 0.0);
-  world.add(make_shared<sphere>(point3(4,1,0),1,material3));
+  world.add(make_shared<sphere>(point3(0, 1, 0), 1, material1));
+
+  auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+  world.add(make_shared<sphere>(point3(-4, 1, 0), 1, material2));
+
+  auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+  world.add(make_shared<sphere>(point3(4, 1, 0), 1, material3));
 
   return world;
 }
@@ -80,12 +82,16 @@ int main() {
   const int samples_per_pixel = 500;
   const int max_depth = 50;
 
+
+  color pixel_matrix[image_height][image_width];
+  //omp_set_num_threads(4);
+  /*std::cerr << "max threads" << omp_get_max_threads() << std::endl;
+  std::cerr << "num threads" << omp_get_num_threads() << std::endl;*/
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
   // World
 
   hittable_list world = random_scene();
-
 
   // Camera
 
@@ -99,12 +105,13 @@ int main() {
              dist_to_focus);
 
   // Render
-
+  #pragma omp parallel for
   for (int j = image_height - 1; j >= 0; j--) {
-    std::cerr.precision(3);
+    /*std::cerr.precision(3);
     std::cerr << "\rRendering: "
-              << double(image_height - j) * 100.0 / double(image_height) << "% "
-              << std::flush;
+              << double(image_height - j) * 100.0 / double(image_height)
+              << "%"
+              << std::flush;*/
     for (int i = 0; i < image_width; i++) {
       color pixel_color = color(0, 0, 0);
       for (int s = 0; s < samples_per_pixel; s++) {
@@ -113,7 +120,14 @@ int main() {
         ray r = cam.get_ray(u, v);
         pixel_color += ray_color(r, world, max_depth);
       }
-      write_color(std::cout, pixel_color, samples_per_pixel);
+      pixel_matrix[j][i] = pixel_color;
+      // write_color(std::cout, pixel_color, samples_per_pixel);
+    }
+  }
+
+  for (int j = image_height - 1; j >= 0; j--) {
+    for (int i = 0; i < image_width; i++) {
+      write_color(std::cout, pixel_matrix[j][i], samples_per_pixel);
     }
   }
   std::cerr << "\nDone.\n";
