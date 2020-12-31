180a099e4c74c469baae810b122c14cfb1c7cdeb
Matheus Cavazotti <mcavazotti@gmail.com>
2020-12-31 08:29:33 -0300

    first image CUDA

--- InOneWeekendCUDA/main.cu
+++ InOneWeekendCUDA/main.cu
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 #include "cuda_utils.h"
 
+__global__ void render(float *fb, int maxX, int maxY) {
+    int i = threadIdx.x + blockIdx.x + blockDim.x;
+    int j = threadIdx.y + blockIdx.y + blockDim.y;
+    if((i >= maxX) || (j >= maxY)) return;
+    
+    int pixelIdx = j*maxX*3 + i*3;
+    fb[pixelIdx] = float(i) / maxX;
+    fb[pixelIdx + 1] = float(j) / maxY;
+    fb[pixelIdx + 2] = 0.2;
+    
+     
+}
+
+
 int main(int argc, char const *argv[]){
     if( argc < 3){
         std::cerr << "Missing arguments." << std::endl
@@ -15,6 +29,37 @@ int main(int argc, char const *argv[]){
     std:: cerr << "Rendering a " << imageWidth << "x" << imageHeight << " image in "
                 << BLOCK_X << "x" << BLOCK_Y << " blocks.\n";
 
+    // Allocate frame buffer
+    int numPixels = imageHeight * imageWidth;
+    size_t frameBufferSize = 3* numPixels * sizeof(float);
+
+    float *frameBuffer;
+
+    checkCudaErrors(cudaMallocManaged(void **)&frameBuffer, frameBufferSize);
+
+    // Render
+    dim3 blocks(imageWidth/BLOCK_X+1,imageHeight/BLOCK_Y+1);
+    dim3 threads(BLOCK_X,BLOCK_Y);
+
+    render<<<blocks,threads>>>(frameBuffer, imageWidth, imageHeight);
+    checkCudaErrors(cudaGetLastError());
+    checkCudaErrors(cudaDeviceSynchronize());
+
+    std::cout << "P3\n" << imageWidth << " " << imageHeight <<"\n255\n";
+    for(int j = imageHeight-1; j >= 0; j--){
+        for(int i = 0; i < imageWidth; i++){
+            size_t pixelIdx = 3*j*imageWidth + i*3;
+            float r = frameBuffer[pixelIdx];
+            float g = frameBuffer[pixelIdx + 1];
+            float b = frameBuffer[pixelIdx + 2];
+
+            int ir = int(255.99*r);
+            int ig = int(255.99*g);
+            int ib = int(255.99*b);
+            std::cout << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+    checkCudaErrors(cudaFree(frameBuffer));
 
     return 0;
 }
