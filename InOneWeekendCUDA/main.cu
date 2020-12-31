adc84047c5734425d0fc42eea192c6b5fe9b5744
Matheus Cavazotti <mcavazotti@gmail.com>
2020-12-31 10:06:54 -0300

    More changes

--- InOneWeekendCUDA/main.cu
+++ InOneWeekendCUDA/main.cu
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <stdlib.h>
 #include "cuda_utils.h"
+#include "vec3.h"
 
-__global__ void render(float *fb, int maxX, int maxY) {
-    int i = threadIdx.x + blockIdx.x + blockDim.x;
-    int j = threadIdx.y + blockIdx.y + blockDim.y;
+__global__ void render(vec3 *fb, int maxX, int maxY) {
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
     if((i >= maxX) || (j >= maxY)) return;
     
-    int pixelIdx = j*maxX*3 + i*3;
-    fb[pixelIdx] = float(i) / maxX;
-    fb[pixelIdx + 1] = float(j) / maxY;
-    fb[pixelIdx + 2] = 0.2;
+    int pixelIdx = j*maxX + i;
+    fb[pixelIdx][0] = float(i) / maxX;
+    fb[pixelIdx][1] = float(j) / maxY;
+    fb[pixelIdx][2] = 0.2;
     
      
 }
@@ -33,9 +34,9 @@ int main(int argc, char const *argv[]){
 
     // Allocate frame buffer
     int numPixels = imageHeight * imageWidth;
-    size_t frameBufferSize = 3* numPixels * sizeof(float);
+    size_t frameBufferSize = numPixels * sizeof(vec3);
 
-    float *frameBuffer;
+    vec3 *frameBuffer;
 
     checkCudaErrors(cudaMallocManaged((void **)&frameBuffer, frameBufferSize));
 
@@ -50,10 +51,10 @@ int main(int argc, char const *argv[]){
     std::cout << "P3\n" << imageWidth << " " << imageHeight <<"\n255\n";
     for(int j = imageHeight-1; j >= 0; j--){
         for(int i = 0; i < imageWidth; i++){
-            size_t pixelIdx = 3*j*imageWidth + i*3;
-            float r = frameBuffer[pixelIdx];
-            float g = frameBuffer[pixelIdx + 1];
-            float b = frameBuffer[pixelIdx + 2];
+            size_t pixelIdx = j*imageWidth + i;
+            float r = frameBuffer[pixelIdx][0];
+            float g = frameBuffer[pixelIdx][1];
+            float b = frameBuffer[pixelIdx][2];
 
             int ir = int(255.99*r);
             int ig = int(255.99*g);
---
+++ InOneWeekendCUDA/rtweekend.h
@@ -0,0 +1,40 @@
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+
+#include <cmath>
+#include <cstdlib>
+#include <limits>
+#include <memory>
+
+// usings
+using std::make_shared;
+using std::shared_ptr;
+using std::sqrt;
+
+// constants
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// utiliy functions
+__host__ __device__ inline double degree_to_radian(double degrees) {
+  return degrees * pi / 180;
+}
+
+__host__ __device__ inline double random_double() {
+  return rand() / (double(RAND_MAX) + 1);
+}
+
+__host__ __device__ inline double random_double(double min, double max) {
+  return min + (max - min) * random_double();
+}
+
+__host__ __device__ inline double clamp(double x, double min, double max) {
+  if (x < min) return min;
+  if (x > max) return max;
+  return x;
+}
+
+// commom headers
+//#include "ray.h"
+
+#endif
