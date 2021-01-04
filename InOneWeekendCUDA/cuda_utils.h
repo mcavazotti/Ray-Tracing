c611a3ec21f0199b3ec05707e88a2dead571b9ac
Matheus Cavazotti <mcavazotti@gmail.com>
2021-01-04 11:56:38 -0300

    teste 7

--- InOneWeekendCUDA/cuda_utils.h
+++ InOneWeekendCUDA/cuda_utils.h
@@ -5,8 +5,9 @@
 void check_cuda(cudaError_t result, char const *const func,
                 const char *const file, int const line) {
   if (result) {
-    std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at "
-              << file << ":" << line << " '" << func << "' \n";
+    std::cerr << "CUDA error = " << static_cast<unsigned int>(result)
+              << "(" << cudaGetErrorName(result) << ") at " << file << ":" << line << " '" << func
+              << "' \n";
     cudaDeviceReset();
     exit(99);
   }
