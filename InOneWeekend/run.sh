61cd4546c7d9cf8ec6570d6d0708c75bd74b3718
Matheus Cavazotti <mcavazotti@gmail.com>
2021-01-06 17:34:16 -0300

    script update

--- InOneWeekend/run.sh
+++ InOneWeekend/run.sh
@@ -7,18 +7,30 @@ make
 
 rm *.txt
 
+echo "set title \"Tempo de renderização\"" >> time.plg
+echo "set ylabel \"Tempo (ms)\"" >> time.plg
+echo "set xlabel \"Dimensão da imagem\"" >> time.plg
+echo "set yrange [0:]" >> time.plg
+echo "set terminal png size 1080,720" >> time.plg
+echo "set style fill solid" >> time.plg
+echo "set output \"./time.png\"" >> time.plg
+echo "set style data histograms" >> time.plg
+echo "plot '-' using 2:xtic(1) title ''" >> time.plg
+
 for smp in $(seq 1 $SAMPLING_FACTOR)
 do
+    DATA=""
     for s in $(seq 1 $SIZE_FACTOR)
     do
         TOTAL=0
         for r in $(seq 1 $REPETITIONS)
         do
             #echo "$r/$REPETITIONS"
-            TMP=$(./rayTracer $(( 120 * $s )) $(( 80 * $s )) $(( 10* $smp )) 2>&1 >/dev/null | sed '2q;d' | sed -e 's/[^0-9]//g')
+            TMP=$(./rayTracer $(( 120 * $s )) $(( 80 * $s )) $(( 10 * $smp )) 2>&1 >/dev/null | sed '2q;d' | sed -e 's/[^0-9]//g')
             TOTAL=$(( TOTAL + TMP ))
         done
         MEAN=`echo "scale=3; $TOTAL/$REPETITIONS" | bc -l`
-        echo "$(( 120*$s ))x$(( 80*$s ))" $MEAN
     done
+    DATA=$DATA+' '+$MEAN
+    echo "$(( 120*$s ))x$(( 80*$s ))" $DATA
 done
